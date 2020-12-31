458daabfb9ab556d5dc939489c2c4320c58bbf37
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2020-12-31 11:31:12 +0100

    Class Main extends Application

--- src/main/java/fr/pederobien/mumble/client/gui/Main.java
+++ src/main/java/fr/pederobien/mumble/client/gui/Main.java
@@ -1,8 +1,16 @@
 package fr.pederobien.mumble.client.gui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
 	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
 
 	}
 }
