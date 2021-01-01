4b01aa8bc34069359655682415a63dd76e7d3d5d
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2021-01-01 20:25:17 +0100

    Implementing method init

--- src/main/java/fr/pederobien/mumble/client/gui/Main.java
+++ src/main/java/fr/pederobien/mumble/client/gui/Main.java
@@ -1,7 +1,13 @@
 package fr.pederobien.mumble.client.gui;
 
 import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import fr.pederobien.dictionary.impl.DefaultDictionaryParser;
+import fr.pederobien.dictionary.impl.JarDictionaryParser;
+import fr.pederobien.dictionary.impl.NotificationCenter;
+import fr.pederobien.dictionary.interfaces.IDictionaryParser;
 import fr.pederobien.mumble.client.gui.configuration.persistence.GuiConfigurationPersistence;
 import fr.pederobien.mumble.client.gui.impl.Environment;
 import fr.pederobien.mumble.client.gui.impl.presenter.ServerListPresenter;
@@ -33,6 +39,7 @@ public class Main extends Application {
 		}
 
 		ViewBase.setGuiConfiguration(GuiConfigurationPersistence.getInstance().get());
+		registerDictionaries("French.xml");
 	}
 
 	@Override
@@ -48,5 +55,32 @@ public class Main extends Application {
 	@Override
 	public void stop() throws Exception {
 
+	}
+
+	private void registerDictionaries(String... dictionaryNames) {
+		String url = getClass().getResource(getClass().getSimpleName() + ".class").toExternalForm();
+
+		if (url.startsWith("file")) {
+			DefaultDictionaryParser parser = new DefaultDictionaryParser();
+			for (String name : dictionaryNames)
+				registerDictionary(parser, Paths.get(Environment.RESOURCES_FOLDER.getFileName(), name));
+
+		} else if (url.startsWith("jar")) {
+			Path jarPath = Paths.get(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath().substring(1));
+			String internalPath = Environment.RESOURCES_FOLDER.getFileName();
+			JarDictionaryParser parser = new JarDictionaryParser(internalPath);
+			for (String name : dictionaryNames)
+				registerDictionary(parser.setName(internalPath.concat(name)), jarPath);
+
+		} else
+			throw new UnsupportedOperationException("Technical error");
+	}
+
+	private void registerDictionary(IDictionaryParser parser, Path path) {
+		try {
+			NotificationCenter.getInstance().getDictionaryContext().register(parser, path);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 }
