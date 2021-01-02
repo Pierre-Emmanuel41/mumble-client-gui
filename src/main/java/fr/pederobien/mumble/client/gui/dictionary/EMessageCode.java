a8430ca8c4040f9d73b975724055e81f71500df0
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2021-01-02 15:19:55 +0100

    Updating EMessageCode

--- src/main/java/fr/pederobien/mumble/client/gui/dictionary/EMessageCode.java
+++ src/main/java/fr/pederobien/mumble/client/gui/dictionary/EMessageCode.java
@@ -3,7 +3,23 @@ package fr.pederobien.mumble.client.gui.dictionary;
 import fr.pederobien.dictionary.interfaces.IMessageCode;
 
 public enum EMessageCode implements IMessageCode {
-	EMPTY_SERVER_LIST;
+	// Code when there is no registered server.
+	EMPTY_SERVER_LIST,
+
+	// Code to join a server
+	JOIN_SERVER,
+
+	// Code to add a server
+	ADD_SERVER,
+
+	// Code to edit a server
+	EDIT_SERVER,
+
+	// Code to delete a server
+	DELETE_SERVER,
+
+	// Code to refresh servers state
+	REFRESH_SERVERS;
 
 	@Override
 	public String value() {
