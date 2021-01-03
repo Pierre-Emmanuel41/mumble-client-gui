713a6f01d137f2d319735773e5f18a39b2430a4d
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2021-01-03 11:56:25 +0100

    Setting ScrollPane as root for ServerListView

--- src/main/java/fr/pederobien/mumble/client/gui/impl/view/ServerListView.java
+++ src/main/java/fr/pederobien/mumble/client/gui/impl/view/ServerListView.java
@@ -2,22 +2,29 @@ package fr.pederobien.mumble.client.gui.impl.view;
 
 import fr.pederobien.mumble.client.gui.impl.presenter.ServerListPresenter;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.layout.StackPane;
 
-public class ServerListView extends ViewBase<ServerListPresenter, StackPane> {
+public class ServerListView extends ViewBase<ServerListPresenter, ScrollPane> {
 	private Label emptyServerListLabel;
 
 	public ServerListView(ServerListPresenter presenter) {
-		super(presenter, new StackPane());
+		super(presenter, new ScrollPane());
 
-		getRoot().setPrefHeight(500);
-		getRoot().setPrefWidth(1500);
+		getRoot().setHbarPolicy(ScrollBarPolicy.AS_NEEDED);
+		getRoot().setVbarPolicy(ScrollBarPolicy.AS_NEEDED);
+
+		StackPane root = new StackPane();
+		root.setPrefSize(1500, 500);
+
+		getRoot().setContent(root);
 
 		emptyServerListLabel = new Label();
 		emptyServerListLabel.textProperty().bind(getPresenter().emptyServersListLanguageProperty());
 		emptyServerListLabel.fontProperty().bind(getPresenter().emptyServersListFontProperty());
 
 		if (getPresenter().getServers().isEmpty())
-			getRoot().getChildren().add(emptyServerListLabel);
+			root.getChildren().add(emptyServerListLabel);
 	}
 }
