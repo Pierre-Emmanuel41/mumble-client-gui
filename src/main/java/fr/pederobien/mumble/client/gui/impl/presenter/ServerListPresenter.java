b911d48cbbc6b7bd0fc2ad1ad420020bb9bb963c
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2021-01-03 19:07:59 +0100

    Adding display of registered servers in class ServerListView

--- src/main/java/fr/pederobien/mumble/client/gui/impl/presenter/ServerListPresenter.java
+++ src/main/java/fr/pederobien/mumble/client/gui/impl/presenter/ServerListPresenter.java
@@ -1,22 +1,30 @@
 package fr.pederobien.mumble.client.gui.impl.presenter;
 
 import fr.pederobien.mumble.client.gui.dictionary.EMessageCode;
+import fr.pederobien.mumble.client.gui.impl.view.ServerView;
 import fr.pederobien.mumble.client.gui.interfaces.IObsServerList;
 import fr.pederobien.mumble.client.gui.model.Server;
 import fr.pederobien.mumble.client.gui.model.ServerList;
 import fr.pederobien.mumble.client.gui.properties.SimpleFontProperty;
 import fr.pederobien.mumble.client.gui.properties.SimpleLanguageProperty;
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 public class ServerListPresenter extends PresenterBase implements IObsServerList {
-	private ObservableList<Server> servers;
+	private ObservableList<Object> servers;
 	private SimpleLanguageProperty emptyServersListLanguageProperty;
 	private SimpleFontProperty emptyServersListFontProperty;
+	private BooleanProperty emptyServersListVisibilityProperty;
+	private Server selectedServer;
 
 	public ServerListPresenter(Stage primaryStage, ServerList serverList) {
 		super(primaryStage);
@@ -25,22 +33,25 @@ public class ServerListPresenter extends PresenterBase implements IObsServerList
 		servers = FXCollections.observableArrayList(serverList.getServers());
 		emptyServersListLanguageProperty = createLanguageProperty(EMessageCode.EMPTY_SERVER_LIST);
 		emptyServersListFontProperty = createFontProperty();
+		emptyServersListVisibilityProperty = new SimpleBooleanProperty(serverList.getServers().isEmpty());
 	}
 
 	@Override
 	public void onServerAdded(Server server) {
 		servers.add(server);
+		emptyServersListVisibilityProperty.setValue(servers.isEmpty());
 	}
 
 	@Override
 	public void onServerRemoved(Server server) {
 		servers.remove(server);
+		emptyServersListVisibilityProperty.setValue(servers.isEmpty());
 	}
 
 	/**
 	 * @return An observable list that contains all registered servers.
 	 */
-	public ObservableList<Server> getServers() {
+	public ObservableList<Object> getServers() {
 		return servers;
 	}
 
@@ -56,5 +67,51 @@ public class ServerListPresenter extends PresenterBase implements IObsServerList
 	 */
 	public ObjectProperty<Font> emptyServersListFontProperty() {
 		return emptyServersListFontProperty;
+	}
+
+	/**
+	 * @return The property to display the message when there is no registered server.
+	 */
+	public BooleanProperty emptyServersListVisibilityProperty() {
+		return emptyServersListVisibilityProperty;
+	}
+
+	/**
+	 * @return The factory in order to get the view associated to a server.
+	 */
+	public Callback<ListView<Object>, ListCell<Object>> serverCellFactory() {
+		return listView -> {
+			return new ListCell<Object>() {
+
+				@Override
+				protected void updateItem(Object item, boolean empty) {
+					super.updateItem(item, empty);
+
+					if (empty || item == null) {
+						setText(null);
+						setGraphic(null);
+					} else
+						setGraphic(new ServerView(new ServerPresenter(getPrimaryStage(), (Server) item)).getRoot());
+				}
+			};
+		};
+	}
+
+	/**
+	 * Action to perform when the selected server has changed.
+	 * 
+	 * @param oldServer The old selected server.
+	 * @param newServer The new selected server.
+	 */
+	public void onServerSelectedChanged(Object oldServer, Object newServer) {
+		this.selectedServer = (Server) newServer;
+		System.out.println("Selected server : " + selectedServer);
+	}
+
+	/**
+	 * @return The server currently selected in the list.
+	 */
+	public Server getSelectedServer() {
+		return selectedServer;
 	}
 }
--- src/main/java/fr/pederobien/mumble/client/gui/impl/view/ServerListView.java
+++ src/main/java/fr/pederobien/mumble/client/gui/impl/view/ServerListView.java
@@ -2,9 +2,11 @@ package fr.pederobien.mumble.client.gui.impl.view;
 
 import fr.pederobien.mumble.client.gui.impl.presenter.ServerListPresenter;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.layout.FlowPane;
 
 public class ServerListView extends ViewBase<ServerListPresenter, ScrollPane> {
 	private Label emptyServerListLabel;
@@ -15,16 +17,23 @@ public class ServerListView extends ViewBase<ServerListPresenter, ScrollPane> {
 		getRoot().setHbarPolicy(ScrollBarPolicy.AS_NEEDED);
 		getRoot().setVbarPolicy(ScrollBarPolicy.AS_NEEDED);
 
-		StackPane root = new StackPane();
-		root.setPrefSize(1500, 500);
-
+		FlowPane root = new FlowPane();
 		getRoot().setContent(root);
 
 		emptyServerListLabel = new Label();
 		emptyServerListLabel.textProperty().bind(getPresenter().emptyServersListLanguageProperty());
 		emptyServerListLabel.fontProperty().bind(getPresenter().emptyServersListFontProperty());
+		emptyServerListLabel.visibleProperty().bind(getPresenter().emptyServersListVisibilityProperty());
 
 		if (getPresenter().getServers().isEmpty())
 			root.getChildren().add(emptyServerListLabel);
+
+		ListView<Object> listView = new ListView<Object>(getPresenter().getServers());
+		listView.visibleProperty().bind(getPresenter().emptyServersListVisibilityProperty().not());
+		listView.setCellFactory(getPresenter().serverCellFactory());
+		listView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+		listView.getSelectionModel().selectedItemProperty().addListener((obs, oldValue, newValue) -> getPresenter().onServerSelectedChanged(oldValue, newValue));
+
+		root.getChildren().add(listView);
 	}
 }
