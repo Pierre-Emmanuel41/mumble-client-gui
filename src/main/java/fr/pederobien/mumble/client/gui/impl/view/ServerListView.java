df2ba04e47bc44586e7ad417e9cc28bc9a1e88c6
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2021-01-04 10:57:55 +0100

    Updating class ServerListView

--- src/main/java/fr/pederobien/mumble/client/gui/impl/view/ServerListView.java
+++ src/main/java/fr/pederobien/mumble/client/gui/impl/view/ServerListView.java
@@ -3,37 +3,30 @@ package fr.pederobien.mumble.client.gui.impl.view;
 import fr.pederobien.mumble.client.gui.impl.presenter.ServerListPresenter;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.control.SelectionMode;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 
-public class ServerListView extends ViewBase<ServerListPresenter, ScrollPane> {
-	private Label emptyServerListLabel;
+public class ServerListView extends ViewBase<ServerListPresenter, StackPane> {
 
 	public ServerListView(ServerListPresenter presenter) {
-		super(presenter, new ScrollPane());
+		super(presenter, new StackPane());
+		getRoot().setBackground(new Background(new BackgroundFill(Color.MAGENTA, null, null)));
 
-		getRoot().setHbarPolicy(ScrollBarPolicy.AS_NEEDED);
-		getRoot().setVbarPolicy(ScrollBarPolicy.AS_NEEDED);
-
-		FlowPane root = new FlowPane();
-		getRoot().setContent(root);
-
-		emptyServerListLabel = new Label();
+		Label emptyServerListLabel = new Label();
 		emptyServerListLabel.textProperty().bind(getPresenter().emptyServersListLanguageProperty());
 		emptyServerListLabel.fontProperty().bind(getPresenter().emptyServersListFontProperty());
 		emptyServerListLabel.visibleProperty().bind(getPresenter().emptyServersListVisibilityProperty());
-
-		if (getPresenter().getServers().isEmpty())
-			root.getChildren().add(emptyServerListLabel);
+		getRoot().getChildren().add(emptyServerListLabel);
 
 		ListView<Object> listView = new ListView<Object>(getPresenter().getServers());
+		listView.setMaxWidth(600);
 		listView.visibleProperty().bind(getPresenter().emptyServersListVisibilityProperty().not());
 		listView.setCellFactory(getPresenter().serverCellFactory());
 		listView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 		listView.getSelectionModel().selectedItemProperty().addListener((obs, oldValue, newValue) -> getPresenter().onServerSelectedChanged(oldValue, newValue));
-
-		root.getChildren().add(listView);
+		getRoot().getChildren().add(listView);
 	}
 }
