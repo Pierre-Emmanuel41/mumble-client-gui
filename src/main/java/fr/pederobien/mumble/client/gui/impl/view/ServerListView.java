a1f34d48ec55d4bd8bb26ec4bab3322f47a486a8
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2021-01-04 11:27:38 +0100

    Removing magenta background

--- src/main/java/fr/pederobien/mumble/client/gui/impl/view/ServerListView.java
+++ src/main/java/fr/pederobien/mumble/client/gui/impl/view/ServerListView.java
@@ -4,16 +4,12 @@ import fr.pederobien.mumble.client.gui.impl.presenter.ServerListPresenter;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 
 public class ServerListView extends ViewBase<ServerListPresenter, StackPane> {
 
 	public ServerListView(ServerListPresenter presenter) {
 		super(presenter, new StackPane());
-		getRoot().setBackground(new Background(new BackgroundFill(Color.MAGENTA, null, null)));
 
 		Label emptyServerListLabel = new Label();
 		emptyServerListLabel.textProperty().bind(getPresenter().emptyServersListLanguageProperty());
