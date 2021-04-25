76b8a14f13de0c9c49129a9d354e0830241f621b
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2021-04-25 12:27:54 +0200

    Adding display of player mute state in PlayerChannelView

--- src/main/java/fr/pederobien/mumble/client/gui/impl/presenter/PlayerChannelPresenter.java
+++ src/main/java/fr/pederobien/mumble/client/gui/impl/presenter/PlayerChannelPresenter.java
@@ -1,19 +1,27 @@
 package fr.pederobien.mumble.client.gui.impl.presenter;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import fr.pederobien.mumble.client.gui.environment.Environments;
+import fr.pederobien.mumble.client.gui.environment.Variables;
 import fr.pederobien.mumble.client.interfaces.IChannel;
 import fr.pederobien.mumble.client.interfaces.IOtherPlayer;
 import fr.pederobien.mumble.client.interfaces.observers.IObsPlayer;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.scene.image.Image;
 
 public class PlayerChannelPresenter extends PresenterBase implements IObsPlayer {
 	private static final Map<String, PlayerChannelPresenter> PRESENTERS = new HashMap<String, PlayerChannelPresenter>();
 
 	private PlayerPresenter playerPresenter;
 	private StringProperty playerNameProperty;
+	private BooleanProperty isPlayerMute;
+	private Image muteImage;
 
 	public static PlayerChannelPresenter getOrCreatePlayerPresenter(PlayerPresenter playerPresenter, IOtherPlayer player) {
 		PlayerChannelPresenter presenter = PRESENTERS.get(player.getName());
@@ -28,6 +36,13 @@ public class PlayerChannelPresenter extends PresenterBase implements IObsPlayer 
 	private PlayerChannelPresenter(PlayerPresenter playerPresenter, IOtherPlayer player) {
 		this.playerPresenter = playerPresenter;
 		playerNameProperty = new SimpleStringProperty(player.getName());
+		isPlayerMute = new SimpleBooleanProperty(false);
+
+		try {
+			muteImage = Environments.loadImage(Variables.MICROPHONE_OFF.getFileName());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -47,7 +62,7 @@ public class PlayerChannelPresenter extends PresenterBase implements IObsPlayer 
 
 	@Override
 	public void onMuteChanged(boolean isMute) {
-
+		dispatch(() -> isPlayerMute.set(isMute));
 	}
 
 	/**
@@ -55,5 +70,13 @@ public class PlayerChannelPresenter extends PresenterBase implements IObsPlayer 
 	 */
 	public StringProperty playerNameProperty() {
 		return playerNameProperty;
+	}
+
+	public Image getMuteImage() {
+		return muteImage;
+	}
+
+	public BooleanProperty isPlayerMute() {
+		return isPlayerMute;
 	}
 }
--- src/main/java/fr/pederobien/mumble/client/gui/impl/view/PlayerChannelView.java
+++ src/main/java/fr/pederobien/mumble/client/gui/impl/view/PlayerChannelView.java
@@ -2,15 +2,23 @@ package fr.pederobien.mumble.client.gui.impl.view;
 
 import fr.pederobien.mumble.client.gui.impl.presenter.PlayerChannelPresenter;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 
-public class PlayerChannelView extends ViewBase<PlayerChannelPresenter, GridPane> {
+public class PlayerChannelView extends ViewBase<PlayerChannelPresenter, BorderPane> {
 
 	public PlayerChannelView(PlayerChannelPresenter presenter) {
-		super(presenter, new GridPane());
+		super(presenter, new BorderPane());
 
 		Label playerName = new Label();
 		playerName.textProperty().bind(getPresenter().playerNameProperty());
-		getRoot().add(playerName, 0, 0);
+		getRoot().setLeft(playerName);
+
+		ImageView muteView = new ImageView(getPresenter().getMuteImage());
+		muteView.visibleProperty().bind(getPresenter().isPlayerMute());
+		muteView.managedProperty().bind(getPresenter().isPlayerMute());
+		muteView.setPreserveRatio(true);
+		muteView.setFitHeight(15);
+		getRoot().setRight(muteView);
 	}
 }
