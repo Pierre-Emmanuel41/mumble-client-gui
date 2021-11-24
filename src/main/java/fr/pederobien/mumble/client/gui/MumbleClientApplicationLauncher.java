db5c9a6c350571e5f5fbdab7942b7634ac0f4989
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2021-11-24 20:01:10 +0100

    Fix: Application cannot be run from jar file

---
+++ src/main/java/fr/pederobien/mumble/client/gui/MumbleClientApplicationLauncher.java
@@ -0,0 +1,13 @@
+package fr.pederobien.mumble.client.gui;
+
+public class MumbleClientApplicationLauncher {
+
+	/**
+	 * Fake main in order to run the application from a jar file.
+	 * 
+	 * @param args the command line arguments passed to the application.
+	 */
+	public static void main(String[] args) {
+		MumbleClientApplication.main(args);
+	}
+}
