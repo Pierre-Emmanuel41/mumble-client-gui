47b8fcf42b3cd763a6d16ade4a3e1e281b77e0d8
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2021-11-27 23:56:36 +0100

    Moving application setup in class MumbleClientApplicationLauncher

--- src/main/java/fr/pederobien/mumble/client/gui/MumbleClientApplicationLauncher.java
+++ src/main/java/fr/pederobien/mumble/client/gui/MumbleClientApplicationLauncher.java
@@ -1,5 +1,21 @@
 package fr.pederobien.mumble.client.gui;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import fr.pederobien.communication.event.ConnectionEvent;
+import fr.pederobien.mumble.client.event.PlayerSpeakEvent;
+import fr.pederobien.mumble.client.gui.environment.Variables;
+import fr.pederobien.sound.event.SoundEvent;
+import fr.pederobien.utils.event.EventCalledEvent;
+import fr.pederobien.utils.event.EventLogger;
+
 public class MumbleClientApplicationLauncher {
 
 	/**
@@ -8,6 +24,33 @@ public class MumbleClientApplicationLauncher {
 	 * @param args the command line arguments passed to the application.
 	 */
 	public static void main(String[] args) {
+		EventLogger.instance().ignore(SoundEvent.class).ignore(ConnectionEvent.class).ignore(PlayerSpeakEvent.class).register();
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> saveLog()));
 		MumbleClientApplication.main(args);
+	}
+
+	private static void saveLog() {
+		// Creates intermediate folders if they don't exist.
+		if (!Files.exists(Variables.LOG_FOLDER.getPath()))
+			Variables.LOG_FOLDER.getPath().toFile().mkdirs();
+
+		String name = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss").format(LocalDateTime.now());
+		Path logPath = Variables.LOG_FOLDER.getPath().resolve(String.format("log_%s.zip", name));
+
+		try {
+			ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(logPath.toFile()));
+			ZipEntry zipEntry = new ZipEntry(String.format("log_%s.txt", name));
+			zipOutputStream.putNextEntry(zipEntry);
+
+			for (EventCalledEvent event : EventLogger.instance().getEvents()) {
+				String entry = String.format("[%s %s] %s\r\n", event.getTime().toLocalDate(), event.getTime().toLocalTime(), event.getEvent().toString());
+				zipOutputStream.write(entry.getBytes());
+			}
+
+			zipOutputStream.closeEntry();
+			zipOutputStream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/main/java/fr/pederobien/mumble/client/gui/impl/presenter/MainPresenter.java
+++ src/main/java/fr/pederobien/mumble/client/gui/impl/presenter/MainPresenter.java
@@ -1,35 +1,16 @@
 package fr.pederobien.mumble.client.gui.impl.presenter;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
-
-import fr.pederobien.communication.event.ConnectionEvent;
 import fr.pederobien.mumble.client.gui.dictionary.EMessageCode;
-import fr.pederobien.mumble.client.gui.environment.Variables;
 import fr.pederobien.mumble.client.gui.impl.view.ServerListView;
 import fr.pederobien.mumble.client.gui.impl.view.ServerManagementView;
 import fr.pederobien.mumble.client.gui.persistence.configuration.GuiConfigurationPersistence;
 import fr.pederobien.mumble.client.gui.persistence.model.ServerListPersistence;
 import fr.pederobien.mumble.client.interfaces.IMumbleServer;
-import fr.pederobien.sound.event.SoundEvent;
-import fr.pederobien.utils.event.EventCalledEvent;
 import fr.pederobien.utils.event.EventLogger;
 
 public class MainPresenter extends PresenterBase {
 	private ServerListView serverListView;
 	private ServerManagementView serverManagementView;
-
-	static {
-		EventLogger.instance().register();
-		EventLogger.instance().ignore(SoundEvent.class).ignore(ConnectionEvent.class);
-		Runtime.getRuntime().addShutdownHook(new Thread(() -> saveLog()));
-	}
 
 	public MainPresenter() {
 		setPrimaryStageTitle(EMessageCode.MUMBLE_WINDOW_TITLE);
@@ -60,30 +41,5 @@ public class MainPresenter extends PresenterBase {
 	 */
 	public ServerManagementView getServerManagementView() {
 		return serverManagementView;
-	}
-
-	private static void saveLog() {
-		// Creates intermediate folders if they don't exist.
-		if (!Files.exists(Variables.LOG_FOLDER.getPath()))
-			Variables.LOG_FOLDER.getPath().toFile().mkdirs();
-
-		String name = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss").format(LocalDateTime.now());
-		Path logPath = Variables.LOG_FOLDER.getPath().resolve(String.format("log_%s.zip", name));
-
-		try {
-			ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(logPath.toFile()));
-			ZipEntry zipEntry = new ZipEntry(String.format("log_%s.txt", name));
-			zipOutputStream.putNextEntry(zipEntry);
-
-			for (EventCalledEvent event : EventLogger.instance().getEvents()) {
-				String entry = String.format("[%s %s] %s\r\n", event.getTime().toLocalDate(), event.getTime().toLocalTime(), event.getEvent().toString());
-				zipOutputStream.write(entry.getBytes());
-			}
-
-			zipOutputStream.closeEntry();
-			zipOutputStream.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 }
